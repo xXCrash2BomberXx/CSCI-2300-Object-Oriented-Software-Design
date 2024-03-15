ae238ef7de9f9248bebb7c493bdc42fe8dcb9459
bleesman21 <bradley.leesman@slu.edu>
2024-03-14 21:15:50 -0500

    Class Files

--- circle2d-bleesman21/Driver.java
+++
@@ -1,6 +0,0 @@
-public class Driver {
-    public static void main (String[] args) {
-        System.out.println(new Circle2D());
-        System.out.println(new Circle2D(new Point2D(1, 1), 10));
-    }
-}
